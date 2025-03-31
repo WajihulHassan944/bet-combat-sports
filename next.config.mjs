f098c3d88a1327b6851a874be91ff6ed50fd644d
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2025-03-31 19:37:05 +0500

    added next folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone', // Ensure proper deployment on Vercel
   webpack: (config) => {
     config.ignoreWarnings = [/Failed to parse source map/];
     return config;
